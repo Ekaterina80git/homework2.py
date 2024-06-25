43f3a6a480fc6f6f040355e20075650b249fd2fc
Ekaterina80git <timofeewa.yekat@yandex.ru>
2024-06-25 15:05:13 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,13 @@
+homework =(12)
+time = (1.5)
+name ="pyhton"
+result = (0.125)
+print(name ,homework ,time ,result)
+
+
+# 2//задача
+first_name = "Ylia"
+apple_count = 15
+second_name = "Ydgin"
+orange_count = 10
+print(first_name ,second_name ,apple_count ,orange_count)
